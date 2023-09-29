0199bd3b4a7d65fc07dcf73441313cc71d773e83
AlbertoSalazar05 <asalbuj20@iesmarquesdecomares.org>
2023-09-29 14:44:58 +0200

    feat add halaMadrid

---
+++ src/ies/marquesDeComares/daw/edd/HalaMadrid.java
@@ -0,0 +1,10 @@
+package ies.marquesDeComares.daw.edd;
+
+public class HalaMadrid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hala Madrid");
+	}
+
+}
