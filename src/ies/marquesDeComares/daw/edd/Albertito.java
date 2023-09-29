967dfa927bc603512de0975b1b29c63c76b840e8
AlbertoSalazar05 <asalbuj20@iesmarquesdecomares.org>
2023-09-29 14:36:16 +0200

    feat add albertito to the new branch

---
+++ src/ies/marquesDeComares/daw/edd/Albertito.java
@@ -0,0 +1,10 @@
+package ies.marquesDeComares.daw.edd;
+
+public class Albertito {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Putos");
+	}
+
+}
