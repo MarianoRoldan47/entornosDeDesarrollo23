c21e6c6c1c9b04a8a22e2e3cfdc4ddba65e99e16
MarianoRoldan47 <118228181+Rolu47@users.noreply.github.com>
2023-10-02 10:37:47 +0200

    fix the best team

--- src/ies/marquesDeComares/daw/edd/HalaMadrid.java
+++ src/ies/marquesDeComares/daw/edd/HalaMadrid.java
@@ -4,7 +4,7 @@ public class HalaMadrid {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hala Madrid");
+		System.out.println("Visca El BarÇa");
 	}
 
 }
