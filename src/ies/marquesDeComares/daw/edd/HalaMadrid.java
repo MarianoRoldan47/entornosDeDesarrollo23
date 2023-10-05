ef039b7b4792dd80aa0ac5a8971ea34497e06205
manologomez123 <145774876+manologomez123@users.noreply.github.com>
2023-10-05 12:26:16 +0200

    Update HalaMadrid.java

--- src/ies/marquesDeComares/daw/edd/HalaMadrid.java
+++ src/ies/marquesDeComares/daw/edd/HalaMadrid.java
@@ -5,6 +5,7 @@ public class HalaMadrid {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Visca El BarÇa");
+		System.out.println("Nuevo cambio realizado");
 	}
 
 }
